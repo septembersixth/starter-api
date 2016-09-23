ae971fb6c6356c47735e2cd57287d934eac2e961
tuan <tuan.tran@wonga.com>
2016-09-23 16:55:33 +0100

    add data

--- services/hello.js
+++ services/hello.js
@@ -1,7 +1,66 @@
 var User = require('../models/user.js');
 
 module.exports = function(req, res) {
-  // res.json({"foo": "cool"});
+  var data = {
+    "data": [
+      {
+        "header": "Before borrowing",
+        "links": [
+          {
+            "title": "Things to consider if you need to borrow money",
+            "url": "/money-to-borrow"
+          },
+          {
+            "title": "About your Personal Application Limit",
+            "url": "/personal-application-limit"
+          },
+          {
+            "title": "What are same day loans and when could they help you?",
+            "url": "/same-day-loans"
+          },
+          {
+            "title": "Applying for Small Loans ?",
+            "url": "/small-loans"
+          }
+        ]
+      },
+
+      {
+        "header": "Before borrowing",
+        "links": [
+          {
+            "title": "Things to consider if you need to borrow money",
+            "url": "/money-to-borrow"
+          },
+          {
+            "title": "About your Personal Application Limit djhdf",
+            "url": "/personal-application-limit"
+          },
+          {
+            "title": "What are same day loans and when could they help you?",
+            "url": "/same-day-loans"
+          },
+          {
+            "title": "Applying for Small Loans ?",
+            "url": "/small-loans"
+          },
+          {
+            "title": "About your Personal Application Limit",
+            "url": "/personal-application-limit"
+          },
+          {
+            "title": "What are same day loans and when could they help you?",
+            "url": "/same-day-loans"
+          },
+          {
+            "title": "Applying for Small Loans ?",
+            "url": "/small-loans"
+          }
+        ]
+      }
+    ]
+  };
+  res.json(data);
 
   // var newUser = new User();
   // newUser.displayName = 'test tutu name';
@@ -14,9 +73,9 @@ module.exports = function(req, res) {
   //   res.json(data);
   // });
 
-  
-  User.findOne({email: 'email test'}).exec()
-    .then(function(data) {
-      res.json(data);
-    });
+
+  // User.findOne({email: 'email test'}).exec()
+  //   .then(function(data) {
+  //     res.json(data);
+  //   });
 };
